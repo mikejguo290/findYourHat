26d73d04a88aef45870880b056ef0e3c24002359
mikejguo290 <mikejguo290@gmail.com>
2021-05-18 17:00:10 +0100

    Add a .print() method that prints the current state of the field.

--- main.js
+++ main.js
@@ -9,4 +9,10 @@ class Field {
     constructor(field){
         this.fieldGrid = field;
     }
+    print(){
+        // print out a string representation of the board. .join('') on each row array to join up elements without spaces in a new string.
+        this.fieldGrid.forEach((fieldRow)=>{
+            console.log( fieldRow.join('') );
+        });
+    }
 };
